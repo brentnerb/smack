3847d134c2d62707878928a64f300d8d20198293
Brent Morgan <brentnerb@gmail.com>
2017-10-23 18:32:24 -0400

    Lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Brent Morgan on 10/23/17.
+//  Copyright © 2017 Brent Morgan. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func dismissPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Brent Morgan on 10/23/17.
+//  Copyright © 2017 Brent Morgan. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
+
+
+
